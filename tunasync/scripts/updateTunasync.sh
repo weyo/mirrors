4bf93174134cf8e993aef0feff67b5b7c100a0e1
Miao Wang <shankerwangmiao@gmail.com>
2020-05-22 12:39:23 +0800

    Clarify the support of multiple mirrors in one tunasync worker.

--- tunasync/scripts/updateTunasync.sh
+++ tunasync/scripts/updateTunasync.sh
@@ -1,17 +1,18 @@
 #! /bin/bash
 source ~/.bash_profile
 
-centos_size=`du -sh /mirrors/centos/ | awk '{print $1}'`
-epel_size=`du -sh /mirrors/epel/ | awk '{print $1}'`
-ubuntu_size=`du -sh /mirrors/ubuntu/ | awk '{print $1}'`
-pypi_size=`du -sh /mirrors/pypi/ | awk '{print $1}'`
+# Not needed because sizes can be retrived directly from rsync logs 
+#centos_size=`du -sh /mirrors/centos/ | awk '{print $1}'`
+#epel_size=`du -sh /mirrors/epel/ | awk '{print $1}'`
+#ubuntu_size=`du -sh /mirrors/ubuntu/ | awk '{print $1}'`
+#pypi_size=`du -sh /mirrors/pypi/ | awk '{print $1}'`
 
-tunasynctl set-size -w centos_worker centos $centos_size
-tunasynctl set-size -w epel_worker epel $epel_size
-tunasynctl set-size -w ubuntu_worker ubuntu $ubuntu_size
-tunasynctl set-size -w pypi_worker pypi $pypi_size
+#tunasynctl set-size -w worker centos $centos_size
+#tunasynctl set-size -w worker epel $epel_size
+#tunasynctl set-size -w worker ubuntu $ubuntu_size
+#tunasynctl set-size -w worker pypi $pypi_size
 
-sleep 5
+#sleep 5
 
 mv /mirrors/jobs.json /mirrors/jobs.json.bak
 wget -c http://localhost:14242/jobs -O /mirrors/jobs.json -o /mirrors/log/plog/wget.log
